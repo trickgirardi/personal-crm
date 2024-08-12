053c9618031480dff5fbb9e1492e167b81b897b5
Patrick Girardi <dev.patrickgirardi@gmail.com>
2024-08-12 16:20:54 -0300

    create first functional version

---
+++ script.js
@@ -0,0 +1,11 @@
+function add() {
+    const ul = document.querySelector("ul")
+    const input = document.querySelector("input")
+    const tarefa = input.value
+
+    const li = document.createElement("li")
+    li.innerText = tarefa
+
+    ul.appendChild(li)
+    input.value = ""
+}
